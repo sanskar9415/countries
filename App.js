4cb9a0a5119485b0e1b39228a72b5b56b19ec188
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-08-12 18:09:37 +0530

    Route setup and code refactoring

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import Header from './components/Header'
-import SearchBar from './components/SearchBar'
-import SelectMenu from './components/SelectMenu'
-import CountriesList from './components/CountriesList'
 import { useState } from 'react'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   const [query , setQuery] = useState('');
   return (
     <>
-      <Header />
-      <main>
-        <div className="search-filter-container">
-          <SearchBar setQuery = {setQuery}/>
-          <SelectMenu />
-        </div>
-        <CountriesList query = {query} />
-      </main>
+    <Header/>
+     <Outlet/>
     </>
   )
 }
--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 import { createRoot } from "react-dom/client";
 import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        errorElement:<Error/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>    
+            },
+            {
+                path: '/contact',
+                element: <Contact/>
+            }
+        ]
+    }
+])
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App/>)
+root.render(<RouterProvider router={router}/>)
